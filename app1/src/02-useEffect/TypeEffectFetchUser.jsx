8ea0781f8d6d312021e3cc94f8e6a1785be48187
santhiyasubramaniam239 <santhiyasubramaniam239@gmail.com>
2025-07-04 20:01:09 +0530

    react

--- app1/src/02-useEffect/TypeEffectFetchUser.jsx
+++ app1/src/02-useEffect/TypeEffectFetchUser.jsx
@@ -1,17 +1,32 @@
 import React,{useState, useEffect} from "react";
 export default function EffectFetchUser(){
     const [userId, setUserId]= useState(1);
+    const[name, setName]=useState('');
+    const[email, setEmail]=useState('');
+    const[company, setCompany] = useState('');
+    
     useEffect(()=>{
         fetch(`https://jsonplaceholder.typicode.com/users/${userId}`)
     .then((res)=>res.json())
     .then((data)=>{
-        console.log("User data fetched:", data.name);
+        
+        setName(data.name);
+        setEmail(data.email);
+        setCompany(data.company.name);
+       
     });
     },[userId]);
+   
     return (
         <div>
             <h3>User ID: {userId}</h3>
-            <button onClick={()=> setUserId(userId+1)}>Next User</button>
+            <button onClick={()=> setUserId(userId+1)} >Next User</button>
+            <div>
+              <p>Name:{name}</p> 
+              <p>Email:{email}</p>
+              <p>Company:{company}</p>
+             </div>
         </div>
+       
     );
 }
--- app1/src/App.jsx
+++ app1/src/App.jsx
@@ -1,53 +1,10 @@
-import Simplecount from "./01-useState/simplecount"
-import PasswordToggle from "./01-useState/PasswordToggle"
-import FormInput from "./01-useState/FormInput"
-import DarkToggle from "./01-useState/DarkToggle"
-import AddTask from "./01-useState/AddTask"
-import ChangeTitle from "./02-useEffect/ChangeTitle"
-import Cleanup from "./02-useEffect/CleanupFunction"
-import EffectCount from "./02-useEffect/EffectCount"
-import TypeEffectEveryRender from "./02-useEffect/TypeEffectEveryRender"
-import EffectOnce from "./02-useEffect/TypeEffectOnce"
-import EffectOnDependency from "./02-useEffect/TypeEffectOnDependency"
-import EffectWithCleanup from "./02-useEffect/TypeEffectWithCleanup"
-import EffectMultipleDeps from "./02-useEffect/TypeEffectMultipleDeps"
+
 import EffectFetchUser from "./02-useEffect/TypeEffectFetchUser"
-import EffectWithEventListener from "./02-useEffect/TypeEffectWithEventListener"
-import EffectWithCondition from "./02-useEffect/TypeEffectWithCondition"
-import UseStateNumber from "./01-useState/TypeUseStateNumber"
-import UseStateString from "./01-useState/TypeUseStateString"
-import UseStateBoolean from "./01-useState/TypeUseStateBoolean"
-import UseStateArray from "./01-useState/TypeUseStateArray"
-import UseStateObject from "./01-useState/TypeUseStateObject"
-import UseStateNestedObject from "./01-useState/TypeUseStateNestedObject"
-import UseStateFunctionInit from "./01-useState/TypeUseStateFunctionInit"
 
 function App() {
 
-  //return <Simplecount/>
-  //return <PasswordToggle/>
-  //return <FormInput/>
-  //return <DarkToggle/>
-  // return <AddTask/>
-  // return <ChangeTitle/>
-  //return <Cleanup/>  
-  //return <EffectCount/>
-  //return <TypeEffectEveryRender/>
-  //return <EffectOnce/>
-  //return <EffectOnDependency/>
-  //return <EffectWithCleanup/>
-  // return <EffectMultipleDeps/>
-  // return <EffectFetchUser/>
-  // return <EffectWithEventListener/>
-  // return <EffectWithCondition/>
-  //return <UseStateNumber/>
-  //return <UseStateString/>
-  //return <UseStateBoolean/>
-  //return <UseStateArray/>
-  //return <UseStateObject/>
-  //return <UseStateNestedObject/>
-  return <UseStateFunctionInit/>
-
+  
+  return <EffectFetchUser/>
 }
 
 export default App
